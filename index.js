0bfe0456e52094cac7a2cc3e74b4c7003c99fa6f
Sherman Burwell <shermanburwell3@gmail.com>
2024-07-19 11:29:46 -0500

    Created file generation for svg

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 const shapes = require("./lib/shapes.js");
-const
+const fs = require('fs');
+
+// Placeholder for text and textColor
+const text = "ABC";
+const textColor = "blue";
+
+// declare a new shape
+const shape = new shapes();
+
+// Set up the xml code for the svg file
+const logo = `<svg version="1.1"
+width="300" height="200"
+xmlns="http://www.w3.org/2000/svg">
+${shape.render(text, textColor)}
+`
+
+//write the shape to a file
+fs.writeFile("test-write.svg", shape.render(text, textColor), function(error) {
+    if (error) {
+        console.log(error);
+    }
+    else {
+
+    }
+});
--- test/shapes.test.js
+++ test/shapes.test.js
@@ -9,9 +9,9 @@ describe('shapes', function () {
         expect(shape.text.length).toBeLessThanOrEqual(3);
         
     });
-    it('should have no more than three letters', function() {
-        const text = "ABC";
-        const textColor = "blue";
+    it('should have uppercase text and color should return as lowercase', function() {
+        const text = "AbC";
+        const textColor = "Blue";
         const shape = new shapes(text, textColor);
 
         expect(shape.render(shape.text, shape.textColor)).toBe(`<text x="10" y="10"><tspan fill="blue">ABC</text>`);
