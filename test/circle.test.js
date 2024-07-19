3dedf94c66657c82dffb30df8e74e27eefa4f4cc
Sherman Burwell <shermanburwell3@gmail.com>
2024-07-19 11:37:17 -0500

    Add test for circle class

--- test/circle.test.js
+++ test/circle.test.js
@@ -2,11 +2,9 @@ const circle = require('../lib/circle.js');
 
 describe('circle', function() {
     it('is a circle', function() {
-        //circle code
+        
+        const nweCircle = new circle;
+        expect(newCircle.render(text, textColor, shapeColor)).toBe(`<circle cx=150" cy="100" r"80" fill="red"/>`);
     });
 
-    it('is not a circle', function() {
-        //circle code that doesnt make a circle
-        //
-    });
-})
+});
