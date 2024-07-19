5174defd4671ddc7d768395b8e68b9374d1f02a7
Sherman Burwell <shermanburwell3@gmail.com>
2024-07-19 12:30:45 -0500

    Initial circle test passed.

--- test/circle.test.js
+++ test/circle.test.js
@@ -1,10 +1,15 @@
-const circle = require('../lib/circle.js');
+const Circle = require('../lib/circle.js');
 
 describe('circle', function() {
     it('is a circle', function() {
         
-        const nweCircle = new circle;
-        expect(newCircle.render(text, textColor, shapeColor)).toBe(`<circle cx=150" cy="100" r"80" fill="red"/>`);
+        const text = "ABC";
+        const textColor = "BLUE";
+        const shapeColor = "Red";
+        const newCircle = new Circle(text, textColor, shapeColor);
+        
+
+        expect(newCircle.render(text, textColor, shapeColor)).toBe(`<circle cx="150" cy="100" r="80" fill="red"/><text x="100" y="120"><tspan font-size="60" fill="blue">ABC</text>`);
     });
 
 });
