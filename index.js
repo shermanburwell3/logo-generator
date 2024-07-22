69c4829fd0da7581dd5ab5e71305dc4264fa8ed8
Sherman Burwell <shermanburwell3@gmail.com>
2024-07-22 14:07:08 -0500

    Added final prompts to index.js, set up writeFile to take all values and write them properly

--- index.js
+++ index.js
@@ -16,6 +16,28 @@ function promptStart(){
         }
     ]).then((response) => {
         textCheck(response.text);
+    }).then(function (response) {
+
+        inquirer.prompt([
+            {
+                type: "list",
+                message: "Please select the shape you wish to use",
+                name: "shape",
+                choices: ["circle", "square", "triangle"],
+            },
+            {
+                type: "input",
+                message: "Please type the color you wish to use for your text or give a hexidecimal value. If the color is not recognized, black will be used",
+                name: "textColor",
+            },
+            {
+                type: "input",
+                message: "Now enter the color you would like the background shape to be, or give a hexidecimal value. If the color is not recognized, black will be used",
+                name: "shapeColor",
+            }
+        ]).then(function(response) {
+            writeSVGFile(response.fileName, response.shape, response.text, response.textColor, response.shapeColor);
+        })
     });
 
 }
@@ -29,26 +51,28 @@ function textCheck(text) {
 
 promptStart();
 
-// Placeholder for text and textColor, remove after prompts are created
-const text = "ABC";
-const textColor = "blue";
+function writeSVGFile(fileName, shape, text, textColor, shapeColor) {
+    // Placeholder for text and textColor, remove after prompts are created
+    const text = "ABC";
+    const textColor = "blue";
 
-// declare a new shape
-const shape = new shapes();
+    // declare a new shape, change to switch case to decide the shape
+    const shape = new shapes();
 
-// Set up the xml code for the svg file
-const logo = `<svg version="1.1"
-width="300" height="200"
-xmlns="http://www.w3.org/2000/svg">
-${shape.render(text, textColor)}
-`;
+    // Set up the xml code for the svg file
+    const logo = `<svg version="1.1"
+    width="300" height="200"
+    xmlns="http://www.w3.org/2000/svg">
+    ${shape.render(text, textColor, shapeColor)}
+    `;
 
-//write the shape to a file
-fs.writeFile("test-write.svg", logo, function(error) {
-    if (error) {
-        console.log(error);
-    }
-    else {
+    //write the shape to a file
+    fs.writeFile(`${fileName}.svg`, logo, function(error) {
+        if (error) {
+            console.log(error);
+        }
+        else {
 
-    }
-});
+        }
+    });
+}
